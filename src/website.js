0390af5db7bce5d60a7fff000952a08f7285b699
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-09-12 19:12:13 +0200

    build: add website

---
+++ src/website.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const demoLinks = require('@microlink/demo-links')
+const browserless = require('browserless')()
+const beautyError = require('beauty-error')
+const cartesian = require('cartesian')
+const { reduce } = require('lodash')
+const pAll = require('p-all')
+
+const { writeFile } = require('./util')
+
+const fileOpts = cartesian({
+  type: ['png'],
+  browser: [undefined, 'safari-dark', 'safari-light']
+})
+
+module.exports = async ({ task, concurrency }) => {
+  const total = Object.keys(demoLinks).length * fileOpts.length
+  let index = 0
+
+  const downloadFiles = reduce(
+    demoLinks,
+    (acc, { url, ...demoLinkOpts }, name) => {
+      const files = fileOpts.map(({ type, browser }) => {
+        const browserSkin =
+          typeof browser === 'string' ? browser.split('-')[1] : undefined
+
+        const dist = `dist/website/${
+          browserSkin ? `browser/${browserSkin}/` : ''
+        }${name.toLocaleLowerCase()}.${type}`
+
+        return async () => {
+          try {
+            task.setProgress(name, ++index, total)
+            const buffer = await browserless.screenshot(url, {
+              disableAnimations: true,
+              type,
+              overlay: { browser },
+              ...demoLinkOpts
+            })
+            return writeFile(buffer, dist)
+          } catch (err) {
+            console.error(url)
+            console.error(beautyError(err))
+          }
+        }
+      })
+      return acc.concat(files)
+    },
+    []
+  )
+  await pAll(downloadFiles, { concurrency })
+}
